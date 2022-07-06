0e90a82c4c555b5048de58772e803d0516c3a604
juanhdzx <102987890+juanhdzx@users.noreply.github.com>
2022-07-05 20:03:39 -0400

    fixed server

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const path =require('path');
+//let __dirname = path.dirname(__filename);
+
+// app.use(express.static(__dirname +'/public'));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://root:Password@hopehack.g8olv.mongodb.net/?retryWrites=true&w=majority');
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
-   console.log("connection succeeded");
+   console.log("connection success");
 })
 var app=express();
 
@@ -15,6 +19,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
    extended: true
 }));
+app.use(express.static(__dirname +'/public'));
+
 
 app.post('/sign_up', function(req,res){
    var name = req.body.name;
@@ -31,14 +37,29 @@ app.post('/sign_up', function(req,res){
    db.collection('details').insertOne(data,function(err, collection){
    if (err) throw err;
       console.log("Record inserted Successfully");
+      return res.redirect('/success');
    });
-   return res.redirect('success.html');
+    // return res.redirect('/success');
 })
+
+
+
+app.get('/success', function (req, res) {
+  res.sendFile(__dirname + "/public/" +  "/success.html");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/" +  "/index.html");
+  });
+
+  app.get('/live', function (req, res) {
+    res.sendFile(__dirname + "/public/" +  "/live.html");
+  });
 
 // respond with status 200
 //confirmation 
 
-app.get('/',function(req,res){
+app.get('/contact',function(req,res){
    res.set({
       'Access-control-Allow-Origin': '*'
    });
